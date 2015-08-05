34ea541d731340581253311bfcd1713c61a88f61
Seth Holladay <me@seth-holladay.com>
2015-08-05 12:58:22 -0400

    Add build tooling.
    Add package.json and LICENSE to make npm happy. Add Gruntfile.js for task running.

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+'use strict';
+
+function taskRunner(grunt) {
+
+    // Task configuration.
+    grunt.initConfig(
+        {
+            // Getting the full node app configuration as an object
+            // which can be used internally.
+            pkg : grunt.file.readJSON('package.json'),
+
+            // Clean configuration, used to wipe out temporary build data,
+            // for more robust and reliable builds.
+            clean : {
+                // options : {
+                // //    'no-write': true  // this does a dry-run (logs but no actual file deletion)
+                // },
+                normal : {
+                    src : [
+                        'report',  // directory created by the test system
+                        'build'    // directory created by the build system
+                    ]
+                }
+            },
+
+            // Watch configuration, used for automatically executing
+            // tasks when saving files in the library.
+            watch : {
+                files : ['**.*'],
+                tasks : ['clean', 'build']
+            },
+
+            concat : {
+                core : {
+                    src  : ['lib/require-config', 'node_modules/alameda/alameda.js'],
+                    dest : 'build/core.js'
+                },
+                base : {
+                    src  : ['lib/a.js', 'lib/b.js'],
+                    dest : 'build/base.js'
+                },
+                sitecues : {
+                    src  : ['build/core.js', 'build/base.js'],
+                    dest : 'build/sitecues.js'
+                }
+            },
+            requirejs : {
+                compile : {
+                    options : {
+                        baseUrl : "path/to/base",
+                        mainConfigFile: "path/to/config.js",
+                        name : "path/to/almond", // assumes a production build using almond
+                        out  : "path/to/optimized.js"
+                    }
+                }
+            }
+        }
+    );
+    // Load the plugin that provides the "clean" task.
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    // Load the plugin that provides the "watch" task.
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    // Load the plugin that provides the "concat" task.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    // Make a new task called "build".
+    grunt.registerTask('build', ['concat:core', 'concat:base', 'concat:sitecues'])
+
+    // Default task, will run if no task is specified.
+    grunt.registerTask('default', ['clean', 'build']);
+
+};
+
+module.exports = taskRunner;
