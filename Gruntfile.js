e8758fbcc2767c946efa4662c038d7b0bb968cd8
Seth Holladay <me@seth-holladay.com>
2015-08-07 19:57:21 -0400

    Finish up work on the module loader.
    sitecues Mobile is now a module loading BEAST! It understand its namespace and receives a Promises/A+ implementation via Prim, and lots of other goodies, with simple configuration and excellent tooling.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,43 @@
 'use strict';
 
-var NAMESPACE = 'sitecues';
+var AMD_RUNTIME_CONFIG_PATH = './lib/require-config.js',
+    AMD_LOADER_PATH = '../node_modules/alameda/alameda',
+    NAMESPACE       = 'sitecues',
+    fs              = require('fs'),
+    amdRuntimeConfig;
+
+// Callback for each file that the RequireJS optimizer (r.js)
+// reads while its tracing dependencies for our build.
+function onBuildRead(moduleName, path, contents) {
+
+    // TODO: Figure out why the RequireJS optimizer r.js is calling
+    //       our onBuildRead callback twice for each file.
+    //       It doesn't seem to care about the return value the
+    //       first time around, but it does the second. Weird.
+
+    // By default, assume we don't want to modify the file.
+    var result = contents;
+
+    if (moduleName === AMD_LOADER_PATH) {
+        amdRuntimeConfig = fs.readFileSync(AMD_RUNTIME_CONFIG_PATH, 'utf8');
+        // Prepend our Alameda runtime configuration to Alameda itself,
+        // so that we can use options like "skipDataMain" in it.
+        result = amdRuntimeConfig + result;
+    }
+
+    // console.log('This is onBuildRead:');
+    // console.log('--------------------');
+    // console.log('Number of arguments :', arguments.length);
+    // console.log('Module name         :', moduleName);
+    // console.log('Path                :', path);
+    // console.log('Contents            :', contents);
+
+    return result;
+}
 
 function taskRunner(grunt) {
+
+    // TODO: Consider using AMDclean in the build process to make the initial download smaller.
 
     // Task configuration.
     grunt.initConfig(
@@ -32,30 +67,42 @@ function taskRunner(grunt) {
                 tasks : ['clean', 'build']
             },
 
-            concat : {
-                setup : {
-                    src  : ['lib/namespace.js', 'lib/require-config.js', 'node_modules/alameda/alameda.js', 'lib/require-hooks.js'],
-                    dest : 'build/setup.js'
-                },
-                sitecues : {  // NOTE: core.js comes from requirejs
-                    src  : ['build/setup.js', 'build/core.js'],
-                    dest : 'build/sitecues.js'
-                }
-            },
+            // concat : {
+
+            // },
+
             requirejs : {
                 core : {
                     options : {
                         // Directory to use as the basis for resolving most other relative paths.
                         baseUrl : "lib",
                         // Module that starts the dependency graph.
-                        name    : 'core-amd',
-                        //create : true,
+                        name    : 'core',
                         // Path to write the final output to.
-                        out : 'build/core.js',
+                        out : 'build/sitecues.js',
                         // Add the require() and define() functions as methods of our namespace,
                         // to avoid conflicts with customer pages.
                         namespace : NAMESPACE,
+                        // Add other files or modules to the build, which are not listed
+                        // as dependencies of the main entry point.
+                        include : [
+                            // Add Alameda, our AMD module loader, so that we can load
+                            // code on-demand at runtime.
+                            AMD_LOADER_PATH
+                        ],
+                        // Run a callback for each file in the build, so that we can modify it
+                        // before it gets written to disk.
+                        onBuildRead : onBuildRead,
+
+                        // TODO: We probably want to add lib/namespace.js with onBuildWrite
+
+                        // Prevent the optimizer from creating empty stub modules for files that
+                        // are included in the build, but don't call define() by themselves.
+                        skipModuleInsertion: true,
+                        // Choose a tool to minify the build.
                         optimize : 'none',
+                        // Configure the Uglify2 minifer. These are only applied if this
+                        // tool is in use by the "optimize" option.
                         uglify2: {
                             // Example of a specialized config. If you are fine
                             // with the default options, no need to specify
@@ -82,13 +129,12 @@ function taskRunner(grunt) {
     // Load the plugin that provides the "watch" task.
     grunt.loadNpmTasks('grunt-contrib-watch');
     // Load the plugin that provides the "concat" task.
-    grunt.loadNpmTasks('grunt-contrib-concat');
+    // grunt.loadNpmTasks('grunt-contrib-concat');
     // Load the plugin that provides the "requirejs" task.
     grunt.loadNpmTasks('grunt-contrib-requirejs');
 
     // Make a new task called "build".
-    // grunt.registerTask('build', ['concat:core', 'concat:base', 'concat:sitecues'])
-    grunt.registerTask('build', ['concat:setup', 'requirejs:core', 'concat:sitecues']);
+    grunt.registerTask('build', ['requirejs:core']);
 
     // Default task, will run if no task is specified.
     grunt.registerTask('default', ['clean', 'build']);
