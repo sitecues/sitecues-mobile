68a1f8e958fb90f863ad63bd7a6dfdfac7fc4c95
Seth Holladay <me@seth-holladay.com>
2016-05-20 21:04:44 -0400

    Tweaks.

--- server/view/helper/fortune.js
+++ server/view/helper/fortune.js
@@ -1,17 +1,22 @@
+'use strict';
+
+const
+    fortunes = [
+        "Heisenberg may have slept here...",
+        "Wanna buy a duck?",
+        "Say no, then negotiate.",
+        "Time and tide wait for no man.",
+        "To teach is to learn.",
+        "Never ask the barber if you need a haircut.",
+        "You will forget that you ever knew me.",
+        "You will be run over by a beer truck.",
+        "Fortune favors the lucky.",
+        "Have a nice day!"
+    ];
+
 function fortune() {
-    var fortunes = [
-            "Heisenberg may have slept here...",
-            "Wanna buy a duck?",
-            "Say no, then negotiate.",
-            "Time and tide wait for no man.",
-            "To teach is to learn.",
-            "Never ask the barber if you need a haircut.",
-            "You will forget that you ever knew me.",
-            "You will be run over by a beer truck.",
-            "Fortune favors the lucky.",
-            "Have a nice day!"
-        ],
-        random = Math.floor(Math.random() * fortunes.length);
+
+    const random = Math.floor(Math.random() * fortunes.length);
 
     return fortunes[random];
 };
